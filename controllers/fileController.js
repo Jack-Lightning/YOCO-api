0e3656df6805c4611749addb7a9f72a68aeeceab
Jatin Sharma <jaters1200@gmail.com>
2023-09-28 00:15:12 +0530

    minor changes

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,14 @@
 const docModel = require('../models/docModel.js');
 
-
 exports.uploadFile = async(req,res)=>{
-
+    const doc = new docModel({
+        name:req.body.name,
+        file:req.body.file
+    });
+    try{
+        const savedDoc = await doc.save();
+        res.status(200).json({message:"success",data:savedDoc});
+    }catch(err){
+        res.json({message:err,data:[]});
+    }
 }

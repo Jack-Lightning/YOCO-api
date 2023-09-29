469a4cda30214e033c1dcd66d77bfb48df0d30b2
Jatin Sharma <jaters1200@gmail.com>
2023-09-29 09:55:35 +0530

    minor changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 //FIle Imports
-const {loginUser , registerUser} = require('../controllers/authController.js');
-const {userLogin} = require('../models/googleuserModel.js');
+const {loginUser , registerUser, googleLoginUser} = require('../controllers/authController.js');
+
 
 //Route Middlewares
 router.route('/login').post(loginUser);
 router.route('/register').post(registerUser);
 
-router.route('/gauth').post(userLogin);
+router.route('/gauth').post(googleLoginUser);
 
 module.exports = router;

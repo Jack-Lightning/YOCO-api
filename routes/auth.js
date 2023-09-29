d8e9027d354b010b1211857dac1ad545618fb3bc
Jatin Sharma <jaters1200@gmail.com>
2023-09-29 09:52:50 +0530

    glogin route

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 //FIle Imports
 const {loginUser , registerUser} = require('../controllers/authController.js');
-
+const {userLogin} = require('../models/googleuserModel.js');
 
 //Route Middlewares
 router.route('/login').post(loginUser);
 router.route('/register').post(registerUser);
+
+router.route('/gauth').post(userLogin);
 
 module.exports = router;
